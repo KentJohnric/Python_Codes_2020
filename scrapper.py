5eb43b4b7164ba28b678a0d8001cf3008dafba19
Kent Johnric Sardina <Sardinakent08@gmail.com>
2020-12-23 15:02:10 +0800

    not finishjed code

--- scrapper.py
+++ scrapper.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 
 URL = 'https://www.amazon.com/DisplayPort-Benfei-Adapter-Gold-Plated-Compatible/dp/B07JFTK8YV/ref=sr_1_1_sspa?dchild=1&keywords=DP+to+HDMI&qid=1608706536&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExOFJHQ1U0VjZPUVlQJmVuY3J5cHRlZElkPUEwODc4NjY2MkFDNDVQRVROR0ZXNyZlbmNyeXB0ZWRBZElkPUEwNDc4MDA2MldLVkhWQkY4WFVYVyZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66'}
+headers = {}
 
 page = requests.get(URL,headers=headers)
 
